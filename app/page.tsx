7efbbaf594aaafc39eaca7f36ab7390cf9c34281
almeidadylan <almeidadylan793@gmail.com>
2026-02-24 19:48:42 -0400

    add header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,23 @@
+"use client";
+
 import Image from "next/image";
+import { useState } from 'react';
+import Header from "./components/header.tsx"
 
 export default function Home() {
+
+  const [count, setCount] = useState(0);
+
   return (
+    <section className="flex-col"> 
+      <Header />
+
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <div>
+        <p>{count} likes</p>
+        <button onClick={() => setCount(count + 1)}>Click me</button>
+      </div>
+
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
           className="dark:invert"
@@ -61,5 +76,7 @@ export default function Home() {
         </div>
       </main>
     </div>
+    </section>
+
   );
 }
